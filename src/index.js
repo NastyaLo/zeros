995dcf89467cf7cf1e11d31c1efa35e3feeb8592
NastyaLo <loyko2802@gmail.com>
2018-09-13 22:28:38 +0300

    Calculating how many zeros in the end of result number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let counter = 0;
+
+    for (let i = 1; i <= number; i++) {
+        let num = i;
+        if(num%5 == 0) {
+            counter++;
+            num = num/5;
+
+            while(num%5 == 0) {
+                counter++;
+                num = num/5;
+            };
+        };
+    };
+
+    return counter;
 }
